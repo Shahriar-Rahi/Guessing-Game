108f6e79409c00a09ec932b3652c938c916c8003
Unknown <md.alshahriarrahi@gmail.com>
2018-10-05 23:17:18 +0600

    Initial stage of uploading in github

---
+++ app/src/main/java/com/shahriar_rahi/guessinggame/DeveloperInfo.java
@@ -0,0 +1,13 @@
+package com.shahriar_rahi.guessinggame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeveloperInfo extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.devinfo);
+    }
+}
